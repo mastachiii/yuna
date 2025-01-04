87817045360660fec33bbd26bcd8d138b1c52d7c
mastachii <mastachii273@gmail.com>
2025-01-04 12:57:15 +0800

    feat: Implemented Express Session

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 const session = require("express-session");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const passport = require("passport");
 const passportConfig = require("./passport/passport");
+require("dotenv").config();
 
 const app = express();
 
 app.set("view engine", "ejs");
+
+// Middleware
+app.use(
+    session({
+        cookie: {
+            maxAge: 300000,
+        },
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: new PrismaSessionStore(new PrismaClient(), {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }),
+    })
+);
 
 app.get("/", (req, res) => res.send("<h1>Hello World</h1>"));
 
