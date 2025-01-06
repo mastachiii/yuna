cdfd90d2c64a761ce0a6ab629c911715bcc77a21
mastachii <mastachii273@gmail.com>
2025-01-06 16:47:54 +0800

    feat: Validation for uploading files

--- controller/fileController.js
+++ controller/fileController.js
@@ -6,50 +6,29 @@ const { body, validationResult, check } = require("express-validator");
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_API_KEY);
 const db = new File();
 
-const validateFile = [
-    check("file")
-        .notEmpty()
-        .custom(value => {
-            const UPLOAD_LIMIT = 50 * 1000000;
+async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(401).send(errors.array());
 
-            console.log(value.size > UPLOAD_LIMIT);
+        const file = decode(req.file.buffer.toString("base64"));
+        const path = `${req.user.id}/${req.file.originalname}`;
 
-            if (value.size > UPLOAD_LIMIT) throw new Error("File can't be larger than 50mb");
-        }),
-];
+        await supabase.storage.from("yuna").upload(path, file);
 
-const addFile = [
-    (req, res, next) => {
-        req.body.file = req.file;
+        const { data } = await supabase.storage.from("yuna").getPublicUrl(path);
 
-        next();
-    },
-    validateFile,
-    async (req, res, next) => {
-        try {
-            const errors = validationResult(req);
-            console.log(errors);
-            if (!errors.isEmpty()) return res.status(401).send(errors.array());
+        await db.addFile({
+            name: req.file.originalname,
+            parentFolderId: req.body.folder,
+            url: data.publicUrl,
+        });
 
-            const file = decode(req.file.buffer.toString("base64"));
-            const path = `${req.user.id}/${req.file.originalname}`;
-
-            await supabase.storage.from("yuna").upload(path, file);
-
-            const { data } = await supabase.storage.from("yuna").getPublicUrl(path);
-
-            await db.addFile({
-                name: req.file.originalname,
-                parentFolderId: req.body.folder,
-                url: data.publicUrl,
-            });
-
-            res.redirect("/");
-        } catch (err) {
-            next(err);
-        }
-    },
-];
+        res.redirect("/");
+    } catch (err) {
+        next(err);
+    }
+};
 
 async function getFile(req, res, next) {
     try {
